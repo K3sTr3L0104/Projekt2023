04b433ad7da43f7bd9091fc065777edcf634cd1d
K3sTr3L0104 <84494408+K3sTr3L0104@users.noreply.github.com>
2023-03-29 23:46:29 +0200

    javascript,html,css valtoztatasok es kiegeszitesek

--- indexjs.js
+++ indexjs.js
@@ -13,9 +13,30 @@ function maxim() {
     document.getElementById("orszag").innerHTML="Oroszország";
 }
 function maique() {
-    document.getElementById("nev").innerHTML="Maxim Mikhaylov";
+    document.getElementById("nev").innerHTML="Maique Reis Nascimento";
     document.getElementById("magassag").innerHTML="182 cm";
     document.getElementById("poszt").innerHTML="Libero";
     document.getElementById("kor").innerHTML="25 év";
     document.getElementById("orszag").innerHTML="Brazília";
+}
+function antoine() {
+    document.getElementById("nev").innerHTML="Antoine Brizard";
+    document.getElementById("magassag").innerHTML="196 cm";
+    document.getElementById("poszt").innerHTML="Feladó";
+    document.getElementById("kor").innerHTML="28 év";
+    document.getElementById("orszag").innerHTML="Franciaország";
+}
+function matey() {
+    document.getElementById("nev").innerHTML="Matey Kaziyski";
+    document.getElementById("magassag").innerHTML="202 cm";
+    document.getElementById("poszt").innerHTML="Szélső ütő";
+    document.getElementById("kor").innerHTML="38 év";
+    document.getElementById("orszag").innerHTML="Bulgária";
+}
+function casey() {
+    document.getElementById("nev").innerHTML="Casey Schouten";
+    document.getElementById("magassag").innerHTML="197 cm";
+    document.getElementById("poszt").innerHTML="Feladó átló";
+    document.getElementById("kor").innerHTML="29 év";
+    document.getElementById("orszag").innerHTML="Kanada";
 }

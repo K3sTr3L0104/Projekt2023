f49202c608663b0a4529ffcc53c9815d2cf26997
K3sTr3L0104 <84494408+K3sTr3L0104@users.noreply.github.com>
2023-03-29 13:36:07 +0200

    bestplayers

---
+++ indexjs.js
@@ -0,0 +1,21 @@
+function nishida() {
+    document.getElementById("nev").innerHTML="Nishida Yuji";
+    document.getElementById("magassag").innerHTML="186 cm";
+    document.getElementById("poszt").innerHTML="Feladó, Szélső ütő";
+    document.getElementById("kor").innerHTML="23 év";
+    document.getElementById("orszag").innerHTML="Japán";
+}
+function maxim() {
+    document.getElementById("nev").innerHTML="Maxim Mikhaylov";
+    document.getElementById("magassag").innerHTML="202 cm";
+    document.getElementById("poszt").innerHTML="Feladó átló";
+    document.getElementById("kor").innerHTML="35 év";
+    document.getElementById("orszag").innerHTML="Oroszország";
+}
+function maique() {
+    document.getElementById("nev").innerHTML="Maxim Mikhaylov";
+    document.getElementById("magassag").innerHTML="182 cm";
+    document.getElementById("poszt").innerHTML="Libero";
+    document.getElementById("kor").innerHTML="25 év";
+    document.getElementById("orszag").innerHTML="Brazília";
+}
